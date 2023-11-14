e98bf546ebaf921dcc74486258717f4fbee4d848
noass <therealfunguyy@mgmail.com>
2023-11-14 09:36:52 +0200

    finished students, index, view, edit, create and delete

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Student;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
